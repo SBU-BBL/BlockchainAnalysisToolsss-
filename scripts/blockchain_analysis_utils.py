7665522b1bd583a9013e44f7588d8bd62e5a5169
Noah Tover <139825756+noah-tover@users.noreply.github.com>
2024-11-27 00:04:28 -0500

    Add pruneDictionaries
    Added pruneDictionaries, better whitespace, and credits

--- scripts/blockchain_analysis_utils.py
+++ scripts/blockchain_analysis_utils.py
@@ -1,3 +1,9 @@
+##########################################################################################################################################
+##########################################################################################################################################
+##############################################           Written by Noah Tover           #################################################
+##########################################################################################################################################
+##########################################################################################################################################
+
 
 # This function creates a dictionary mapping different types of hashes to their highest level hash in the blockchain - most commonly public keys.
 # This ensures we have base truth "clusters" before cluster heuristics.
@@ -13,17 +19,19 @@ def normalizeHashes(transactions_json, vout_asm="vout_scriptPubKey_asm", vout_ad
     df = df.drop_duplicates(subset=vout_asm)
     # pubkey and multisig types are the highest standard hash levels.
     filtered_df = df.loc[df[vout_type].isin(['pubkey', 'multisig'])]
+    
     # Parse public keys from asm
     publickeys = filtered_df[vout_asm].apply(parsePushData)
     # Derive several addresses from the public keys
     newly_defined_addresses = publickeys.apply(deriveUndefinedAddresses)
-    # Create wallet ID dictionary. The key is a one way hash function - this uniquely identifies this hash tree.
+    
+    # Create hash dictionary. The key is a one way hash function - this uniquely identifies this hash tree.
     hash_dictionary = {
         sha256(str(each).encode()).hexdigest(): each
         for each in newly_defined_addresses
     }
     # Add non public keys to the dictionary. Only using addresses as these are the only other standard scripts.  
-    ## TODO: Add support for pay to script hash (for multisig addresses) - requires asm decoding. 
+        ## TODO: Add support for pay to script hash (for multisig addresses) - requires asm decoding. 
     df_pkhashes = df[vout_address][df[vout_address] != 'None'] # NOTE: Using str "None" because applymap changes None to strings :/
     for address in df_pkhashes:
         if any(address in value for value in hash_dictionary.values()):
@@ -32,3 +40,29 @@ def normalizeHashes(transactions_json, vout_asm="vout_scriptPubKey_asm", vout_ad
             hash_dictionary[address] = address  # Add the address to the dictionary
 
     return hash_dictionary
+
+##########################################################################################################################################
+# Merges a list of dictionaries with tuple keys and removes smaller tuples that are subsets of larger ones.
+# Returns a dictionary containing only the largest unique tuples with their corresponding values.
+# Can be used for consolidating clusters and normalized hashes calculated on each chunk of data.
+def prune_dictionaries(list_of_dicts):
+    # Merge all dictionaries into one
+    merged_dict = {}
+    for d in list_of_dicts:
+        merged_dict.update(d)
+    
+    # Create a set of keys to remove.
+    keys_to_remove = set()
+    keys = list(merged_dict.keys())
+    
+    # Iterate through each key to check for smaller tuple subsets
+    for i, key1 in enumerate(keys):
+        for key2 in keys:
+            if key1 != key2 and set(key1).issubset(set(key2)):  # If key1 is a subset of key2
+                keys_to_remove.add(key1)  
+    
+    for key in keys_to_remove:
+        if key in merged_dict:
+            del merged_dict[key]
+    
+    return merged_dict
