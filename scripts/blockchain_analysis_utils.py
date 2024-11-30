0cbda8e2147b537ef16e2496ed11f3b7226e7cb6
Noah Tover <139825756+noah-tover@users.noreply.github.com>
2024-11-30 01:58:37 -0500

    Clearer argument names + simpler interface

--- scripts/blockchain_analysis_utils.py
+++ scripts/blockchain_analysis_utils.py
@@ -8,16 +8,9 @@
 # This function creates a dictionary mapping different types of hashes to their highest level hash in the blockchain - most commonly public keys.
 # This ensures we have base truth "clusters" before cluster heuristics.
 # Assumes data is stored in the expected database schema - see documentation folder
-def normalizeHashes(transactions, vout_asm="vout_scriptPubKey_asm", vout_address="vout_scriptPubKey_address", vout_type="vout_scriptPubKey_type", ):
-    df = transactions
-    df = df[[vout_asm, vout_address, vout_type]]
-    # Drop duplicates based on the 'vout_scriptPubKey_asm' column. This also removes duplicate addresses.
-    df = df.drop_duplicates(subset=vout_asm)
-    # pubkey and multisig types are the highest standard hash levels.
-    filtered_df = df.loc[df[vout_type].isin(['pubkey', 'multisig'])]
-    
-    # Derive several addresses from the public keys
-    newly_defined_addresses = publickeys.apply(deriveUndefinedAddresses)
+def normalizeHashes(unique_pubkeys):
+    # Derive all conventional address types from each public key. 
+    newly_defined_addresses = unique_pubkeys.apply(deriveUndefinedAddresses)
 
     # Create a dictionary to map each hash to a unique ID
     hash_dictionary = {}    
