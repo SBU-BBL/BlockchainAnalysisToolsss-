046017d025ac4556f372ede78739aced060cf9fd
Noah Tover <139825756+noah-tover@users.noreply.github.com>
2024-12-03 15:47:42 -0500

    Add fillOutputHashes

--- scripts/sql scripts/parseJsonToDB.py
+++ scripts/sql scripts/parseJsonToDB.py
@@ -70,3 +70,57 @@ def parse_json_to_db(file_path, db_path):
         # Commit changes at the end
         conn.commit()
     conn.close()
+#########################################################################################################################################################################
+# This function fills in the output hashes table by parsing the descriptor when necessary
+def fillOutputHashes(db_path):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # Grab chunks from outputs table
+    batch_size = 5000  # Process rows in batches
+    offset = 0
+    
+    while True:
+        # Fetch all rows with well defined hashes
+        # TODO: Add more supported type
+        cursor.execute(f"""
+            SELECT txid, vout_n, vout_scriptPubKey_address, vout_scriptPubKey_desc, vout_scriptPubKey_type
+            FROM outputs
+            WHERE vout_scriptPubKey_type IN ('pubkey', 'pubkeyhash', 'multisig')
+            LIMIT {batch_size} OFFSET {offset};
+        """)
+        rows = cursor.fetchall()
+
+        if not rows:
+            print(offset, 'to', offset + batch_size, 'rows processed', sep = ' ')
+            break  # Stop the loop when all rows have been processed.
+
+        for row in rows:
+            txid, vout_n, address, desc, scriptPubKey_type = row
+
+            if address:
+                # If address exists, add it
+                addresses_to_use = [address]
+            else:
+                # Parse the descriptor to get the address or list of addresses
+                parsed_result = parseDesc(desc)
+                # Multisig descriptors have a list of keys, for this exception multiple rows should exist in output_hashes to conform to normal form.
+                if isinstance(parsed_result, list) and scriptPubKey_type == "multisig":
+                    addresses_to_use = parsed_result  # Multiple addresses for multisig
+                else:
+                    addresses_to_use = [parsed_result]  # Turn single address to list to make it generalizable.
+
+            # Insert rows into the output_hashes table for each address
+            for addr in addresses_to_use:
+                cursor.execute("""
+                    INSERT INTO output_hashes (txid, vout_n, address)
+                    VALUES (?, ?, ?);
+                """, (txid, vout_n, addr))
+
+        # Commit updates after processing each batch
+        conn.commit()
+        offset += batch_size
+
+    conn.close()
+
+fillOutputHashes(db_path = r"E:\transactions_database")
