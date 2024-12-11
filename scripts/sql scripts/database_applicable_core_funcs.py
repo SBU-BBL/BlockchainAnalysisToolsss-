062754a81803b3ba9efcc634097bcc5d2b770979
Noah Tover <139825756+noah-tover@users.noreply.github.com>
2024-12-11 15:02:27 -0500

    Document dependencies - will format better as  a downloadable library later

--- scripts/sql scripts/database_applicable_core_funcs.py
+++ scripts/sql scripts/database_applicable_core_funcs.py
@@ -86,6 +86,7 @@ def fillOutputHashes(db_path):
 fillOutputHashes(db_path = r"E:\transactions_database")
 ########################################################################################################################
 # TODO: Make these functions general.
+# DEPENDENCIES: normalizeHashes function, import bitcoinlib, pandas as pd, deriveUndefinedAddresses function
 def fillNormalizedHashes(db_path):
     connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
