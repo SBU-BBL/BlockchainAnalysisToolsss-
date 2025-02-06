c0378d772b779b7676544201774bd93d206e7ec8
Noah Tover <139825756+noah-tover@users.noreply.github.com>
2025-02-06 18:01:15 -0500

    Update database_applicable_core_funcs.py

--- scripts/sql scripts/database_applicable_core_funcs.py
+++ scripts/sql scripts/database_applicable_core_funcs.py
@@ -3,7 +3,20 @@ import bitcoinlib
 import pandas as pd
 import re
 
-
+# Grabs the push data in between all the OP_ calls in the asm field, stores the results in a list
+# This is intended to be used for parsing public keys from pubkey and multisig transcations.
+# If individuals_in_list argument is set to False, single public keys will be returned as strings. By default, all returns are in lists.
+def parsePushData(script_asm, individuals_in_list = True):
+  # TODO: Add option for inputs.
+  if not isinstance(script_asm, str):
+    script_asm = str(script_asm)
+  substrings = script_asm.split()
+    # Subsets portion of hex before commands
+  push_data = [substring for substring in substrings if not substring.startswith('OP')]
+  if individuals_in_list == False:
+    if len(push_data) == 1:
+      push_data = push_data[0] # Unlist single public key
+  return push_data
 def parseDesc(descriptor: str):
     # Remove whitespace
     descriptor = descriptor.replace(" ", "")
