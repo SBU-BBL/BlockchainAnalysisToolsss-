4b245b773ec53a3e216025a24f40c8659b323639
Noah Tover <139825756+noah-tover@users.noreply.github.com>
2025-04-14 15:08:40 -0400

    Update create_tables.sql

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,56 +1,47 @@
 CREATE TABLE transactions (
     txid TEXT PRIMARY KEY,
-    median_blocktime TIMESTAMP NULL,
-    miner_time TIMESTAMP NULL,
+    median_blocktime BIGINT NULL,
+    miner_time BIGINT NULL, -- Postgre doesn't allow unix in timestamps. This is for ease of import.
     locktime TEXT NULL
 );
 
 CREATE TABLE outputs (
     txid TEXT NOT NULL,
-    vout_n INTEGER NOT NULL,
+    vout_n REAL NOT NULL,
     vout_value REAL,
-    vout_scriptPubKey_desc TEXT,
-    vout_scriptPubKey_address TEXT,
-    vout_scriptPubKey_type TEXT,
-    vout_wallet_ID varchar(256),
+    vout_scriptPubKey_desc text NULL,
+    vout_scriptPubKey_address text NULL,
+    vout_scriptPubKey_type text,
+    vout_wallet_ID text NULL,
     PRIMARY KEY (txid, vout_n),
     FOREIGN KEY (txid) REFERENCES transactions (txid)
 );
-
-
+-- We are ignoring multisigs for now. Ignore this table.
 CREATE TABLE output_hashes (
     txid TEXT NOT NULL,
-    vout_n INTEGER NOT NULL,
-    vout_scriptPubKey_address TEXT NOT NULL,
-    vout_scriptPubKey_type TEXT NOT NULL,
-    PRIMARY KEY (txid, vout_n, vout_scriptPubKey_address),
-    FOREIGN KEY (txid, vout_n) REFERENCES outputs (txid, vout_n)
+    vout_n REAL NOT NULL,
+    vout_scriptPubKey_address TEXT NULL,
+    vout_scriptPubKey_type TEXT NULL
 );
 
+-- (vin_txid, vin_vout) will be indexed after processing. (vin_txid, vin_vout) contains nulls because of coinbase transactions - but non nulls reference outputs (txid, vout_n)
 CREATE TABLE inputs (
-    txid TEXT NOT NULL,
+    txid TEXT NULL,
     vin_txid TEXT NULL,
-    vin_vout INTEGER NULL,
-    vin_wallet_ID TEXT,
+    vin_vout REAL NULL,
     vin_asm TEXT NULL,
-    witness_data TEXT NULL,
-    PRIMARY KEY (txid, vin_txid, vin_vout),
-    FOREIGN KEY (vin_txid, vin_vout) REFERENCES outputs (txid, vout_n)
+    witness_data TEXT NULL
 );
-
+-- Please keep in mind that the hash may or may not exist in the vout_scriptPubKey_address column as it is derived. Thus, a foreign key isnt used (this is a bandaid)
 CREATE TABLE normalized_hashes (
     hash TEXT NOT NULL,
-    root_hash TEXT NOT NULL
+    root_hash TEXT NOT NULL,
+    PRIMARY KEY (hash, root_hash)
 );
 
 CREATE TABLE cs_clusters (
-	root_hash TEXT NOT NULL,
-	cluster TEXT NOT NULL,
-	FOREIGN KEY (root_hash) REFERENCES normalized_hashes (root_hash)
-);
-
-CREATE TABLE temporal_clusters (
-	root_hash TEXT NOT NULL,
-	cluster TEXT NOT NULL,
-	FOREIGN KEY (root_hash) REFERENCES normalized_hashes (root_hash)
+    root_hash TEXT NULL,
+    cluster TEXT NULL,
+    PRIMARY KEY (root_hash, cluster)
+    FOREIGN KEY (root_hash) REFERENCES normalized_hashes (root_hash)
 );
