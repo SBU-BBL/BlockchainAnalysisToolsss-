eebaa6c919a89b3ef0f93c304e0f6a3b23cbed6b
Noah Tover <139825756+noah-tover@users.noreply.github.com>
2025-04-28 19:11:26 -0400

    Update core_functions_tests.py

--- tests/core_functions_tests.py
+++ tests/core_functions_tests.py
@@ -46,3 +46,74 @@ class testParseDesc(unittest.TestCase):
         desc = "sh(wpkh(02f9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f9))#abcd1234"
         expected = ["02f9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f9"]
         self.assertEqual(parseDesc(desc), expected)
+
+
+class TestDeriveUndefinedAddresses(unittest.TestCase):
+    
+    def test_compressed_pubkey(self):
+        compressed_key = "02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277"
+        expected = [
+            '04e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277f9638aec40320be2eab90a7c245f2a40e248f7748f9991ea121eea23a0498d12',
+            '02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277',
+            '1MHk5o3iammySYyowWcmRPTtAiQAfkhJnS',
+            '13mKVN2PVGYdNLSLG8egVXwnPFrSUtWCTE',
+            'bc1qreglehq5h6dpfzas4tkfr9ltglyrwahmg0g5r6'
+        ]
+        result = deriveUndefinedAddresses(compressed_key)
+        self.assertEqual(result, expected)
+
+    def test_uncompressed_pubkey(self):
+        uncompressed_key = "042f90074d7a5bf30c72cf3a8dfd1381bdbd30407010e878f3a11269d5f74a58788505cdca22ea6eab7cfb40dc0e07aba200424ab0d79122a653ad0c7ec9896bdf"
+        expected = [
+            '042f90074d7a5bf30c72cf3a8dfd1381bdbd30407010e878f3a11269d5f74a58788505cdca22ea6eab7cfb40dc0e07aba200424ab0d79122a653ad0c7ec9896bdf',
+            '032f90074d7a5bf30c72cf3a8dfd1381bdbd30407010e878f3a11269d5f74a5878',
+            '1Fz5s6qVFwP3MDGeNav4ESQXFMpm8ELzUw',
+            '1G2szusZxDPNvLfdLmoY8XqZDKFKDpZ6Hj',
+            'bc1q5njdl3f9axvaaekma22ven2dxld9fjdeaxa898'
+        ]
+        result = deriveUndefinedAddresses(uncompressed_key)
+        self.assertEqual(result, expected)
+
+    def test_multisig_pubkey_assume_owned_true(self):
+        multisig_keys = [
+            "022afc20bf379bc96a2f4e9e63ffceb8652b2b6a097f63fbee6ecec2a49a48010e",
+            "03a767c7221e9f15f870f1ad9311f5ab937d79fcaeee15bb2c722bca515581b4c0"
+        ]
+        expected = [
+            '042afc20bf379bc96a2f4e9e63ffceb8652b2b6a097f63fbee6ecec2a49a48010e2cb891c04968f2b98e4d803100c01206b2974eae23873aac5bb6769964fec510',
+            '022afc20bf379bc96a2f4e9e63ffceb8652b2b6a097f63fbee6ecec2a49a48010e',
+            '1FpModLreXkvXJNM8gbjDn8YMcvXsynVct',
+            '1Bt8XZ3RDUUsRmmqM26uCfNxQF6SEyrjvt',
+            'bc1qwawvzs3lp99xk0wg4qqkq0n27ey3pdm53pmnc4',
+            '04a767c7221e9f15f870f1ad9311f5ab937d79fcaeee15bb2c722bca515581b4c02fb838c9066bd0448ba3c0e734ab987e970b42f40c5b8571a05adcd7d94c72e5',
+            '03a767c7221e9f15f870f1ad9311f5ab937d79fcaeee15bb2c722bca515581b4c0',
+            '1JziWdpY7H8y5hDmCCCoY9Go1WE49SrLyB',
+            '12ppVrt7pVMQnVpekHmrcEZ5vUnUcFfV6w',
+            'bc1qzsp4xvdgf2xeh277vcu306824t9n3e2k8e43jm'
+        ]
+        result = deriveUndefinedAddresses(multisig_keys, assume_multisig_owned=True)
+        self.assertEqual(result, expected)
+
+    def test_multisig_pubkey_assume_owned_false(self):
+        multisig_keys = [
+            "022afc20bf379bc96a2f4e9e63ffceb8652b2b6a097f63fbee6ecec2a49a48010e",
+            "03a767c7221e9f15f870f1ad9311f5ab937d79fcaeee15bb2c722bca515581b4c0"
+        ]
+        expected = [
+            [
+                '042afc20bf379bc96a2f4e9e63ffceb8652b2b6a097f63fbee6ecec2a49a48010e2cb891c04968f2b98e4d803100c01206b2974eae23873aac5bb6769964fec510',
+                '022afc20bf379bc96a2f4e9e63ffceb8652b2b6a097f63fbee6ecec2a49a48010e',
+                '1FpModLreXkvXJNM8gbjDn8YMcvXsynVct',
+                '1Bt8XZ3RDUUsRmmqM26uCfNxQF6SEyrjvt',
+                'bc1qwawvzs3lp99xk0wg4qqkq0n27ey3pdm53pmnc4'
+            ],
+            [
+                '04a767c7221e9f15f870f1ad9311f5ab937d79fcaeee15bb2c722bca515581b4c02fb838c9066bd0448ba3c0e734ab987e970b42f40c5b8571a05adcd7d94c72e5',
+                '03a767c7221e9f15f870f1ad9311f5ab937d79fcaeee15bb2c722bca515581b4c0',
+                '1JziWdpY7H8y5hDmCCCoY9Go1WE49SrLyB',
+                '12ppVrt7pVMQnVpekHmrcEZ5vUnUcFfV6w',
+                'bc1qzsp4xvdgf2xeh277vcu306824t9n3e2k8e43jm'
+            ]
+        ]
+        result = deriveUndefinedAddresses(multisig_keys, assume_multisig_owned=False)
+        self.assertEqual(result, expected)
